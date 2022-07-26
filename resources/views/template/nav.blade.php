aec4ba6024b5817d4c7feda1b76a450c2047ab7d
rivalseptianl <rivalsl639@gmail.com>
2022-07-26 11:34:51 +0700

    Tugas laravel

---
+++ resources/views/template/nav.blade.php
@@ -0,0 +1,7 @@
+<nav class="nav bg-dark">
+    <a class="nav-link active text-white" aria-current="page" href="#">Ujikom2022</a>
+    <a class="nav-link text-white" href="#">Link</a>
+    <a class="nav-link text-white" href="#">Link</a>
+    <a class="nav-link text-white">Keranjang</a>
+    <a class="nav-link text-white">Chekout</a>
+  </nav>
